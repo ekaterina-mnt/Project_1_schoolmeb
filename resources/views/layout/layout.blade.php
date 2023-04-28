82529f9ee76425cc3bab64ddf193983cbf541650
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2023-04-28 20:49:50 +0300

    Refinement

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('public/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('/app.css') }}">
     <link rel="icon" href="{{ asset('public/m.ico') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>@yield('title')</title>
@@ -20,14 +20,19 @@
         @yield('sub-header')
     </div>
     <div class="wrapper">
+
+        @isset ($leftbar)
         @if ($leftbar == 'on')
         <aside class="leftbar">
             @yield('leftbar')
         </aside>
         @endif
+        @endisset
+
         <main class="content">
             @yield('content')
         </main>
+
     </div>
     <footer class="footer">
         <div class="center">
